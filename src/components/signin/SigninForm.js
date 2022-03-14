f091ce99f401f349ee29c2f397e5962fe4f255bf
banlonjones <banlonjones@go-groups.net>
2022-03-14 12:28:22 +0100

    signin page

---
+++ src/components/signin/SigninForm.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import BtnPrimary from '../buttons/Btn-primary';
+
+const SigninForm = () => (
+  <div>
+    <form>
+      <div>
+        <span> Email </span>
+        <br />
+        <input type="text" className="form-control" id="email" required />
+      </div>
+      <div className="mt-2">
+        <span> Password </span>
+        <br />
+        <input type="password" className="form-control" id="email" required />
+      </div>
+      <div className="mt-4">
+        <BtnPrimary value="Login" />
+      </div>
+    </form>
+  </div>
+);
+
+export default SigninForm;
